2e70bd822434645bdb784b32d3100e4a42eaebe7
lalit-machra <lalitmachra0403@gmail.com>
2025-03-10 22:54:34 +0530

    Implement extra credit functions

--- linked-list.js
+++ linked-list.js
@@ -26,17 +26,15 @@ class LinkedList {
         console.log("linked list has no node with next property as null");
       }
     }
+    return node1;
   }
 
   prepend(value) {
     let node2 = new Node();
     node2.value = value;
-    if (this.head === null) {
-      this.head = node2;
-    } else {
-      node2.next = this.head;
-      this.head = node2;
-    }
+    node2.next = this.head;
+    this.head = node2;
+    return node2;
   }
 
   size() {
@@ -129,5 +127,59 @@ class LinkedList {
       temp = temp.next;
     }
     return listString;
+  }
+
+  insertAt(value, index) {
+    let size = this.size();
+    if (this.head === null || index === 0) {   // Can't add node to any other position than 0th index in empty list
+      return this.prepend(value);
+    } else if (index >= size) {   // If new node to be inserted at last index or greater than that, just append it
+      return this.append(value);
+    }
+
+    let ind = 0;
+    let temp = this.head;
+    while (temp !== null) {   // Only required to add new node in between two nodes, other cases are already dealt with
+      if ((index - 1) === ind) {
+        let node3 = new Node();
+        node3.value = value;
+        node3.next = temp.next;
+        temp.next = node3;
+        return node3;
+      }
+      ind++;
+      temp = temp.next;
+    }
+    // Faulty values of index
+    return null;
+  }
+
+  removeAt(index) {
+    let size = this.size();
+    if (this.head === null || index >= size) return null;
+
+    let ind = 0;
+    let removedNode;
+    let temp = this.head;
+    while (temp !== null) {
+      if (index === ind && ind === 0) {   // node at 0th index
+        removedNode = temp;
+        this.head = temp.next;
+        temp.next = null;
+        return removedNode;
+      } else if (index === (size - 1)) {   // remove last node
+        return this.pop();
+      } else {   // remove node from middle indices
+        if ((index - 1) === ind) {
+          removedNode = temp.next;
+          temp.next = temp.next.next;
+          return removedNode;
+        }
+      }
+      ind++;
+      temp = temp.next;
+    }
+    // Faulty values of index
+    return null;
   }
 }
